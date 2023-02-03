d00b92ec184bd16ae186df45e319397ea0d1686a
Clinelly <swall289@gmail.com>
2023-02-03 13:56:43 +0000

    Add functionality for admin comment management

--- tavern/admin.py
+++ tavern/admin.py
@@ -6,4 +6,12 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
+    list_display = ('slug', 'created_on')
+    search_fields = ['author', 'content']
+    prepopulated_fields = {'slug': ('author',)}
+    list_filter = ('created_on', 'updated_on')
     summernote_fields = ('content')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
