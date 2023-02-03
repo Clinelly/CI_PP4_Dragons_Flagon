1ad941731d544d2f0d086c04a8b1416e13200d88
Clinelly <swall289@gmail.com>
2023-02-03 14:13:26 +0000

    Add commentlist to views.py

--- tavern/views.py
+++ tavern/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Comment
 
 # Create your views here.
+
+
+class CommentList(generic.ListView):
+    model = Comment
+    queryset = Comment.objects.filter(approved=True).order_by('created_on')
+    template_name = '#'
+    paginate_by = '#'
