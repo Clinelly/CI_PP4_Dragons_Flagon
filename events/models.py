7744bb3c2433e7985ff85513b48f1b0e795bf534
Clinelly <swall289@gmail.com>
2023-03-17 13:58:34 +0000

    Tidy code in events models

--- events/models.py
+++ events/models.py
@@ -1,9 +1,16 @@
+# Create your models here.
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
 from django.urls import reverse
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(
+        max_length=200
+        )
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
