eaf34a6786f513e1e0aeb9b2ea382a2bcec3ce0a
Clinelly <swall289@gmail.com>
2023-03-17 13:57:32 +0000

    Tidy code in booking models.

--- booking/models.py
+++ booking/models.py
@@ -1,7 +1,11 @@
+# Create your models here.
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
-# Create your models here.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 SERVICE_CHOICES = (
     ("Drinks", "Drinks"),
@@ -25,13 +29,29 @@ TIME_CHOICES = (
 
 
 class TableBooking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+        )
     email = models.EmailField()
-    service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="Drinks")
+    service = models.CharField(
+        max_length=50,
+        choices=SERVICE_CHOICES,
+        default="Drinks"
+        )
     phone = models.IntegerField()
-    day = models.DateField(default=datetime.now)
-    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="12 PM")
-    time_booked = models.DateTimeField(default=datetime.now, blank=True)
+    day = models.DateField(
+        default=datetime.now
+        )
+    time = models.CharField(
+        max_length=10,
+        choices=TIME_CHOICES,
+        default="12 PM")
+    time_booked = models.DateTimeField(
+        default=datetime.now,
+        blank=True)
 
     def __str__(self):
         return f"{self.user.username} | day: {self.day} | time: {self.time}"
