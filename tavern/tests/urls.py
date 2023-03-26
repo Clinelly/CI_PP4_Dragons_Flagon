256328878eaea82a70e99a41a3b89ffdfbf5f929
Clinelly <swall289@gmail.com>
2023-03-26 15:56:25 +0000

    Add tests to tavern app urls

--- tavern/tests/urls.py
+++ tavern/tests/urls.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
-from tavern.models import Review, Gallery, Comment
-from django.contrib.auth.models import User
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from tavern.views import *
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_resolve_to_home_page_view(self):
+        url = reverse('home')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ReviewList)
+
+    def test_resolve_to_review_page_view(self):
+        url = reverse('review_page')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, ReviewPage)
+
+    def test_resolve_to_about_page_view(self):
+        url = reverse('about')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, about)
+
+    def test_resolve_to_gallery_page_view(self):
+        url = reverse('gallery')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, GalleryList)
+
+    def test_resolve_to_food_page_view(self):
+        url = reverse('fooddrink')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, FoodMenu)
+
+    def test_resolve_to_games_page_view(self):
+        url = reverse('games')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, GameList)
+
+    def test_resolve_to_gallery_detail_page_view(self):
+        url = reverse('gallery_detail', args=['slug'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, GalleryDetail)
+
+    def test_resolve_to_gallery_like_view(self):
+        url = reverse('gallery_like', args=['slug'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, GalleryLike)
