b6d98829cd3e6c9578fb07e0e5cd6957977f8da1
Clinelly <swall289@gmail.com>
2023-03-29 22:13:59 +0000

    Fix code error in settings

--- dragonsflagon/settings.py
+++ dragonsflagon/settings.py
@@ -103,8 +103,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dragonsflagon.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
